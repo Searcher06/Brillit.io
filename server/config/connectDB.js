a614b149b6a060dbd1cb7e30c47f918c7df0f5ea
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-05-07 04:10:05 +0100

    made small changes

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const connectDB = async (DATABASE_URI) => {
     try {
         await mongoose.connect(DATABASE_URI)
-        console.log(`Database Connected Successfully ... 😎`)
+        console.log(`Database Connected Successfully ... 😊°C`)
     } catch (error) {
         console.log(error)
     }
