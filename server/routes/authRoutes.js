ecc6300615108ce7ac4d64ebc25b4695902e4435
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-05-08 05:07:00 +0100

    made small changes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,8 +2,11 @@ import { Router } from "express";
 import { signUp, signIn, signOut } from "../controllers/auth.controllers.js";
 const authRouter = Router()
 
+
 authRouter.post('/sign-up', signUp)
+
 authRouter.post('/sign-in', signIn)
+
 authRouter.post('/sign-out', signOut)
 
 export default authRouter
