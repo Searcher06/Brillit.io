d636bae0bb6b01e019e92192e753293446ff443e
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-05-11 01:00:16 +0100

    removed the routes protection

--- server/routes/videosRoutes.js
+++ server/routes/videosRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express'
 import { videoSearch } from '../controllers/videosSearch.js'
 import { videoId } from '../controllers/videoID.js'
-import protect from '../middlewares/authMiddlware.js'
+// import protect from '../middlewares/authMiddlware.js'
 const router = express.Router()
 
-router.get('/search', protect, videoSearch)
+router.get('/search', videoSearch)
 
-router.get('/:id', protect, videoId)
+router.get('/:id', videoId)
 
 export default router
