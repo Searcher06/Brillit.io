d4fbf3d329cd58fd8106322186aed3ed62bfa9bd
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-05-19 21:21:27 +0100

    made a tiny change

--- server/lib/typeSenseHealth.js
+++ server/lib/typeSenseHealth.js
@@ -3,8 +3,8 @@ import client from "../config/typesenseClient.js";
 (async () => {
     try {
         const health = await client.health.retrieve();
-        console.log('Typesense cloud connection is good:', health);
+        console.log('Typesense cloud connection is good and ready to go😎:', health);
     } catch (error) {
-        console.error('Failed to connect to Typesense cloud:', error);
+        console.error('Failed to connect to Typesense cloud 💀:', error);
     }
 })();
