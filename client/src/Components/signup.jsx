c887a3ea2e1b3dba42d3681943a53ccdd58aea2c
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-06-08 22:36:14 +0100

    sign up page built

--- client/src/Components/signup.jsx
+++ client/src/Components/signup.jsx
@@ -1,6 +1,9 @@
 import { FcGoogle } from 'react-icons/fc'
-import { User, Mail, Lock } from 'lucide-react'
+import { useState } from 'react'
+import { User, Mail, Lock, Eye, EyeOff } from 'lucide-react'
 const SignUp = () => {
+    const [show, setShow] = useState(false)
+
     return <div className="w-full h-lvh flex justify-center items-center">
         <form className="w-90 h-120  shadow-xl rounded-lg flex flex-col items-center text-center border border-gray-200">
             <h2 className="text-3xl text-blue-700 font-semibold mt-4">Brillit.io</h2>
@@ -20,12 +23,20 @@ const SignUp = () => {
             </div>
             <div className='mt-3 h-11 w-65 border-gray-200 border-[1.9px] rounded-[8px] flex items-center'>
                 <Mail size={20} className='ml-4' />
-                <input className='outline-0 pl-2 text-sm' placeholder='Email' />
+                <input className='outline-0 pl-2 text-sm w-full' placeholder='Email' />
             </div>
             <div className='mt-3 h-11 w-65 border-gray-200 border-[1.9px] rounded-[8px] flex items-center'>
                 <Lock size={20} className='ml-4' />
-                <input className='outline-0 pl-2 text-sm' placeholder='Password' />
+                <input type={`${show ? 'text' : 'password'}`} className='outline-0 pl-2 text-sm' placeholder='Password' />
+                {show ? <Eye size={17} className='ml-5' onClick={() => {
+                    setShow((prevState) => !prevState)
+                }} /> : <EyeOff size={17} className='ml-5' onClick={() => {
+                    setShow((prevState) => !prevState)
+                }} />}
             </div>
+            <button className='h-11 w-65 bg-blue-700 rounded-[8px] mt-5 text-white text-[15px]'>
+                Create Account
+            </button>
         </form>
     </div>
 }
