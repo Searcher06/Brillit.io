b8b6caf1ce97ad219ef82813dcd22b8f61a84751
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-07-24 22:01:52 +0100

    speachless

--- server/controllers/videosSearch.js
+++ server/controllers/videosSearch.js
@@ -1,176 +1,127 @@
-import axios from 'axios'
-import dotenv from 'dotenv'
-dotenv.config()
-const key = process.env.API_KEY
-import { videoModel } from '../models/video.model.js'
-import client from '../config/typesenseClient.js'
-import { fetchYouTubeVideos } from '../lib/fetchYoutube.js'
-import { getVideosFromMongo } from '../lib/getVideosfromDB.js'
-
-
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+const key = process.env.API_KEY;
+import { videoModel } from "../models/video.model.js";
+import client from "../config/typesenseClient.js";
+import { fetchYouTubeVideos } from "../lib/fetchYoutube.js";
+import { getVideosFromMongo } from "../lib/getVideosfromDB.js";
+import { Error } from "mongoose";
 
 export const searchVideos = async (req, res) => {
+  // Getting the search query
+  const query = req.query.q;
 
-    // Getting the search query
-    const query = req.query.q
+  // Checking the search query
+  if (!query) {
+    res.status(400);
+    throw new Error("Empty search query");
+  }
 
-    // Checking the search query
-    if (!query) {
-        res.status(400)
-        throw new Error("Empty search query")
-    }
+  try {
+    const searchResults = await client
+      .collections("videos")
+      .documents()
+      .search({
+        q: query,
+        query_by: "title,channel,tags", // if the problem persist remove 'tags'
+        query_by_weights: "5,1,3",
+        sort_by: "views:desc",
+        prefix: "true",
+      });
 
+    // checking if the result is < 2 || nothing (call the youtube api)
+    if (searchResults.hits.length < 3 || null) {
+      const freshVideos = await fetchYouTubeVideos(query, res);
+      let savedVideos = [];
 
+      if (freshVideos[0]) {
+        console.log(
+          `Founded ${freshVideos.length} videos from the youtube response`
+        );
+        const createVideoPromises = freshVideos.map(async (element) => {
+          try {
+            const video = await videoModel.create({
+              youtubeId: element.id,
+              title: element.snippet.title,
+              description: element.snippet.description,
+              channelTitle: element.snippet.channelTitle,
+              channelId: element.snippet.channelId,
+              thumbnails: {
+                default: element.snippet.thumbnails.default.url,
+                medium: element.snippet.thumbnails.medium.url,
+                high: element.snippet.thumbnails.high.url,
+              },
+              tags: element.snippet.tags,
+              category:
+                element.snippet.category === "27"
+                  ? "Education"
+                  : element.snippet.category === "28"
+                  ? "Science and technology"
+                  : "",
+              publishedAt: element.snippet.publishedAt,
+              duration: element.contentDetails.duration,
+              searchTerms: query,
+              curated: true,
+              addedBySearch: true,
+              createdAt: Date.now(),
+            });
 
-    try {
-        const searchResults = await client.collections('videos').documents().search({
-            q: query,
-            query_by: 'title,channel,tags',   // if the problem persist remove 'tags'
-            query_by_weights: "5,1,3",
-            sort_by: 'views:desc',
-            prefix: 'true',
-        })
-
-        // checking if the result is < 2 || nothing (call the youtube api)
-        if (searchResults.hits.length < 3 || null) {
-
-            const freshVideos = await fetchYouTubeVideos(query, res)
-            // let savedVideos;
-
-            // if (freshVideos) {
-            //     savedVideos = await videoModel.insertMany(freshVideos)
-            // }
-
-
-            // if (savedVideos) {
-            //     // formating the fresh videos from the youtube API response
-            //     const typesenseFormat = freshVideos.map((current) => ({
-            //         id: current.id.videoId,
-            //         title: video.snippet.title,
-            //         description: video.snippet.description || '',
-            //         url: `https://youtube.com/watch?v=${current.id.videoId}`,
-            //         channel: current.snippet.channelTitle,
-            //         tags: current.snippet.tags || [],
-            //         createdAt: current.snippet.publishedAt ? new Date(current.snippet.publishedAt).getTime() : Date.now()
-            //     }))
-
-            //     // storing videos in typesense
-            //     const typesenseResult = await client.collections('videos').documents().import(typesenseFormat, { action: 'upsert' })
-            //     console.log(typesenseResult)
-
-            //     // returning the new saved vidoes to the client
-            //     res.status(200).json(savedVideos)
-            // }
-
-            // storing the youtube response in db
-            // const insertingVideos = freshVideos.forEach((element) => {
-            //     videoModel.create({
-            //         youtubeId: element.id,
-            //         title: element.snippet.title,
-            //         description: element.snippet.description,
-            //         channelTitle: element.snippet.channelTitle,
-            //         channelId: element.snippet.channelId,
-            //         thumbnails: element.snippet.thumbnails,
-            //         tags: element.snippet.tags,
-            //         category: element.snippet.category === '27' ? 'Education' : element.snippet.category === '28' ? 'Science and technology' : '',
-            //         publishedAt: element.snippet.publishedAt,
-            //         duration: element.contentDetails.duration,
-            //         searchTerms: query,
-            //         curated: true,
-            //         addedBySearch: true,
-            //         createdAt: Date.now(),
-
-            //     })
-            // })
-
-            let savedVideos = [];
-
-            if (freshVideos[0]) {
-                console.log(`Founded ${freshVideos.length} videos from the youtube response`)
-                const createVideoPromises = freshVideos.map(async (element) => {
-                    try {
-                        const video = await videoModel.create({
-                            youtubeId: element.id,
-                            title: element.snippet.title,
-                            description: element.snippet.description,
-                            channelTitle: element.snippet.channelTitle,
-                            channelId: element.snippet.channelId,
-                            thumbnails: {
-                                default: element.snippet.thumbnails.default.url,
-                                medium: element.snippet.thumbnails.medium.url,
-                                high: element.snippet.thumbnails.high.url,
-                            },
-                            tags: element.snippet.tags,
-                            category: element.snippet.category === '27'
-                                ? 'Education'
-                                : element.snippet.category === '28'
-                                    ? 'Science and technology'
-                                    : '',
-                            publishedAt: element.snippet.publishedAt,
-                            duration: element.contentDetails.duration,
-                            searchTerms: query,
-                            curated: true,
-                            addedBySearch: true,
-                            createdAt: Date.now(),
-                        });
-
-                        savedVideos.push(video);
-                    } catch (err) {
-                        if (err.code === 11000) {
-                            console.log(`Duplicate video skipped: ${element.id}`);
-                        } else {
-                            console.error('Error creating video:', err);
-                        }
-                    }
-                });
-
-                await Promise.all(createVideoPromises);
-
-                if (savedVideos) {
-                    try {
-                        const typesenseFormat = savedVideos.map((current) => ({
-                            id: current.youtubeId,
-                            title: current.title || '',
-                            description: current.description || '',
-                            url: `https://youtube.com/watch?v=${current.youtubeId}`,
-                            channel: current.channelTitle,
-                            tags: current.tags || [],
-                            views: 0,
-                            publishedAt: current.publishedAt ? new Date(current.publishedAt).getTime() : Date.now(),
-                            createdAt: Date.now(),
-                        })
-                        )
-
-                        // storing videos in typesense
-                        const typesenseResult = await client.collections('videos').documents().import(typesenseFormat, { action: 'upsert' })
-                        console.log('Saved to typesense successfully : ', typesenseResult)
-                    } catch (error) {
-                        console.log('Typesense insert error', error)
-                    }
-                }
-
-                return res.status(200).json(savedVideos);
+            savedVideos.push(video);
+          } catch (err) {
+            if (err.code === 11000) {
+              console.log(`Duplicate video skipped: ${element.id}`);
+            } else {
+              console.error("Error creating video:", err);
             }
+          }
+        });
 
+        await Promise.all(createVideoPromises);
 
+        if (savedVideos) {
+          try {
+            const typesenseFormat = savedVideos.map((current) => ({
+              id: current.youtubeId,
+              title: current.title || "",
+              description: current.description || "",
+              url: `https://youtube.com/watch?v=${current.youtubeId}`,
+              channel: current.channelTitle,
+              tags: current.tags || [],
+              views: 0,
+              publishedAt: current.publishedAt
+                ? new Date(current.publishedAt).getTime()
+                : Date.now(),
+              createdAt: Date.now(),
+            }));
 
-        } else {
-            const ids = searchResults.hits.map(hit => hit.document.id)
-
-            const dbVideos = await getVideosFromMongo(ids)
-
-            // const typesenseVid = searchResults.hits.map(hit => hit.document)
-
-            if (dbVideos) {
-                res.status(200).json(dbVideos)
-            }
+            // storing videos in typesense
+            const typesenseResult = await client
+              .collections("videos")
+              .documents()
+              .import(typesenseFormat, { action: "upsert" });
+            console.log("Saved to typesense successfully : ", typesenseResult);
+          } catch (error) {
+            console.log("Typesense insert error", error);
+          }
         }
-    } catch (error) {
-        console.log('search error: ', error)
-        res.status(500)
-        throw new Error(error.message)
+
+        return res.status(200).json(savedVideos);
+      }
+    } else {
+      const ids = searchResults.hits.map((hit) => hit.document.id);
+
+      const dbVideos = await getVideosFromMongo(ids);
+
+      // const typesenseVid = searchResults.hits.map(hit => hit.document)
+
+      if (dbVideos) {
+        res.status(200).json(dbVideos);
+      }
     }
-
-
-
-}
+  } catch (error) {
+    console.log("search error: ", error);
+    res.status(500);
+    throw new Error(error);
+  }
+};
--- server/lib/fetchYoutube.js
+++ server/lib/fetchYoutube.js
@@ -1,44 +1,52 @@
-import dotenv from 'dotenv'
-import axios from 'axios'
-dotenv.config()
-const key = process.env.API_KEY
+import dotenv from "dotenv";
+import axios from "axios";
+dotenv.config();
+const key = process.env.API_KEY;
 
 export const fetchYouTubeVideos = async (query, res) => {
-    try {
-        const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
-            params: {
-                q: query,
-                part: 'id',
-                type: 'video',
-                maxResults: 30,
-                key,
+  try {
+    const response = await axios.get(
+      "https://www.googleapis.com/youtube/v3/search",
+      {
+        params: {
+          q: query,
+          part: "id",
+          type: "video",
+          maxResults: 30,
+          key,
+        },
+      }
+    );
 
-            }
-        })
+    // getting the video ID's
+    const ids = await response.data.items
+      .map((current) => {
+        return current.id.videoId;
+      })
+      .toString();
 
-        // getting the video ID's
-        const ids = await response.data.items.map((current) => {
-            return current.id.videoId
-        }).toString()
+    const response2 = await axios.get(
+      "https://www.googleapis.com/youtube/v3/videos",
+      {
+        params: {
+          part: "snippet,contentDetails",
+          id: ids,
+          key,
+        },
+      }
+    );
 
-        const response2 = await axios.get('https://www.googleapis.com/youtube/v3/videos', {
-            params: {
-                part: 'snippet,contentDetails',
-                id: ids,
-                key,
+    const filtered = await response2.data.items.filter((current) => {
+      return (
+        current.snippet.categoryId === "26" ||
+        current.snippet.categoryId === "27"
+      );
+    });
 
-            }
-        })
-
-        const filtered = await response2.data.items.filter((current) => {
-            return current.snippet.categoryId === '26' || current.snippet.categoryId === '27'
-        })
-
-        return filtered
-    } catch (error) {
-        console.log('fetchYouTubeVideos error', error)
-        res.status(500)
-        return error
-    }
-}
-
+    return filtered;
+  } catch (error) {
+    console.log("Fetch youtube videos error", error);
+    res.status(500);
+    throw new Error(error.message);
+  }
+};
