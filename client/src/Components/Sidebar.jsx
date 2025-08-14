d130c077dfbf86457622c668ab87e3f42002bb24
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-08-14 16:05:39 +0100

    tada!

--- client/src/Components/Sidebar.jsx
+++ client/src/Components/Sidebar.jsx
@@ -45,21 +45,38 @@
 // }
 
 import { Home, Snowflake, User } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 export function Sidebar() {
+  const navigate = useNavigate();
   return (
     <div
       className="h-13  w-full bg-white fixed bottom-0 flex justify-between pl-4 pr-4 
     items-center sm:w-16 sm:h-full sm:flex-col sm:justify-normal sm:gap-10 sm:pt-22 lg:pt-24 xl:pt-24"
     >
-      <div className="flex justify-center flex-col items-center">
+      <div
+        className="flex justify-center flex-col items-center"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
         <Home strokeWidth={1.5} />
         <p className="font-[calibri] text-[13px]">Home</p>
       </div>
-      <div className="flex justify-center flex-col items-center">
+      <div
+        className="flex justify-center flex-col items-center"
+        onClick={() => {
+          navigate("/synthai");
+        }}
+      >
         <Snowflake strokeWidth={1.5} />
         <p className="font-[calibri] text-[13px]">SynthAI</p>
       </div>
-      <div className="flex justify-center flex-col items-center">
+      <div
+        className="flex justify-center flex-col items-center"
+        onClick={() => {
+          navigate("/update-profile");
+        }}
+      >
         <User strokeWidth={1.5} />
         <p className="font-[calibri] text-[13px]">You</p>
       </div>
